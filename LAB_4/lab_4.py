4e4b764dc3c172ab0a377e3e0e1bd2b3881234e9
vikku <vita.201581@yandex.ru>
2024-05-02 01:05:07 +0500

    all files

--- LAB_4/lab_4.py
+++ LAB_4/lab_4.py
@@ -5,6 +5,7 @@ import cv2
 from PIL import Image, ImageTk
 import numpy as np
 from sklearn.cluster import KMeans
+import matplotlib.pyplot as plt
 
 
 class ImageProcessingApp:
@@ -19,8 +20,9 @@ class ImageProcessingApp:
         self.create_menu()
         self.create_image_loading_interface()
         self.create_image_display_interface()
-        self.create_threshold_interface()
+        self.create_threshold_interface1()
         self.create_threshold_interface2()
+        self.create_threshold_interface3()
 
     def create_menu(self):
         menubar = tk.Menu(self.root)
@@ -33,38 +35,22 @@ class ImageProcessingApp:
 
     def create_image_loading_interface(self):
         self.load_image_button = tk.Button(self.root, text="Загрузить изображение", command=self.open_image)
-        self.load_image_button.grid(row=0, column=0, pady=10)
+        self.load_image_button.grid(row=0, column=1, pady=10)
 
     def create_image_display_interface(self):
         self.canvas = tk.Canvas(self.root, width=600, height=600)
         self.canvas.grid(row=1, column=0, columnspan=3)
 
-    def create_threshold_interface(self):
-        self.threshold_frame = tk.LabelFrame(self.root, text="Выбор порога")
-        self.threshold_frame.grid(row=2, column=0, padx=10, pady=10)
+    def create_threshold_interface1(self):
+        self.threshold_frame3 = tk.LabelFrame(self.root, text="ПУНКТ 1.")
+        self.threshold_frame3.grid(row=2, column=0, padx=20, pady=20)
 
-        self.edging = tk.Button(self.threshold_frame, text="Последовательное приближение",
-                                command=self.apply_segmentation)
-        self.edging.grid(row=0, column=0, columnspan=2, pady=5)
-
-        self.method_var = tk.StringVar()
-        self.method_var.set("P-tile")
-
-        methods = ["P-tile", "Mean", "Median"]
-        for i, method in enumerate(methods):
-            tk.Radiobutton(self.threshold_frame, text=method, variable=self.method_var, value=method).grid(row=i + 1,
-                                                                                                           column=0,
-                                                                                                           sticky="w",
-                                                                                                           padx=5)
-
-        self.threshold_scale = tk.Scale(self.threshold_frame, from_=0, to=255, orient=tk.HORIZONTAL, label="Порог")
-        self.threshold_scale.grid(row=len(methods) + 1, column=0, columnspan=2, pady=5)
-
-        self.threshold_button = tk.Button(self.threshold_frame, text="Применить порог", command=self.apply_threshold)
-        self.threshold_button.grid(row=len(methods) + 2, column=0, columnspan=2, pady=5)
+        self.nearing = tk.Button(self.threshold_frame3, text="Алгоритм выделения краев",
+                                 command=self.segment_by_edge_detection)
+        self.nearing.pack()
 
     def create_threshold_interface2(self):
-        self.threshold_frame2 = tk.LabelFrame(self.root, text="Выбор порога")
+        self.threshold_frame2 = tk.LabelFrame(self.root, text="ПУНКТ 2.")
         self.threshold_frame2.grid(row=2, column=1, padx=10, pady=10)
 
         self.threshold_scale2 = tk.Scale(self.threshold_frame2, from_=0, to=100, orient=tk.HORIZONTAL,
@@ -72,20 +58,90 @@ class ImageProcessingApp:
         self.threshold_scale2.set(95)
         self.threshold_scale2.pack()
 
-        self.segment_button = tk.Button(self.threshold_frame2, text="Применить сегментацию",
-                                        command=self.apply_segmentation2)
+        self.segment_button = tk.Button(self.threshold_frame2, text="P-tile",
+                                        command=self.p_tile_segmentation)
         self.segment_button.pack()
 
-    def create_threshold_interface3(self):
-        self.threshold_frame3 = tk.LabelFrame(self.root, text="Выбор порога")
-        self.threshold_frame3.grid(row=2, column=2, padx=10, pady=10)
+        self.edging = tk.Button(self.threshold_frame2, text="Последовательное приближение",
+                                command=self.consistent_nearing_segmentation)
+        self.edging.pack(pady=5)
+        self.kmeans = tk.Button(self.threshold_frame2, text="К-means", command=self.k_means_segmentation)
+        self.kmeans.pack(pady=5)
 
-    def apply_segmentation(self):
+    def create_threshold_interface3(self):
+        self.threshold_frame = tk.LabelFrame(self.root, text="ПУНКТ 3")
+        self.threshold_frame.grid(row=2, column=2, padx=10, pady=10)
+
+
+
+        self.method_var = tk.StringVar()
+        self.method_var.set("P-tile")
+
+        methods = ["Среднее", "Медиана", "(min+max)/2"]
+        for i, method in enumerate(methods):
+            tk.Radiobutton(self.threshold_frame, text=method, variable=self.method_var, value=method).grid(row=i + 1,
+                                                                                                           column=2,
+                                                                                                           sticky="w",
+                                                                                                           padx=5)
+
+        self.threshold_scale = tk.Scale(self.threshold_frame, from_=0, to=255, orient=tk.HORIZONTAL, label="Порог")
+        self.threshold_scale.grid(row=len(methods) + 1, column=2, columnspan=2, pady=5)
+
+        self.threshold_button = tk.Button(self.threshold_frame, text="Адаптивный порог", command=self.apply_threshold)
+        self.threshold_button.grid(row=len(methods) + 2, column=2, columnspan=2, pady=5)
+
+    def open_image(self):
+        self.image_path = filedialog.askopenfilename(filetypes=[("Image Files", "*.png;*.jpg;*.jpeg;*.gif;*.bmp")])
+        if self.image_path:
+            self.image = Image.open(self.image_path)
+            self.display_image()
+
+    def display_image(self, image=None):
+        if image is None and self.image_path:
+            image = Image.open(self.image_path)
+        if image:
+            image = image.resize((600, 600), Image.Resampling.LANCZOS)
+            self.tk_image = ImageTk.PhotoImage(image)
+            self.canvas.create_image(300, 300, image=self.tk_image)
+
+    # пункт 1
+    def segment_by_edge_detection(self):
+        # Преобразование изображения в оттенки серого
+        grayscale_image = np.array(self.image.convert("L"))
+
+        # Применение алгоритма выделения краев
+        edges = cv2.Canny(grayscale_image, 100, 200)  # Пример использования оператора Кэнни
+
+        # Бинаризация краевого изображения
+        binary_image = np.where(edges > 0, 255, 0).astype(np.uint8)
+
+        segmented_image = Image.fromarray(binary_image)
+        self.display_image(segmented_image)
+
+    # пункт P-tile
+    def p_tile_segmentation(self):
+        if self.image is not None:
+            # Преобразование изображения в оттенки серого
+            grayscale_image = cv2.cvtColor(np.array(self.image), cv2.COLOR_RGB2GRAY)
+
+            # Вычисление порога на основе процентиля (P-tile)
+            percentile = self.threshold_scale2.get()
+            threshold = np.percentile(grayscale_image, percentile)
+
+            # Бинаризация изображения по порогу
+            binary_image = np.where(grayscale_image >= threshold, 255, 0).astype(np.uint8)
+
+            # Отображение сегментированного изображения
+            segmented_image = Image.fromarray(binary_image)
+            self.display_image(segmented_image)
+
+    # Последовательные приближения
+    def consistent_nearing_segmentation(self):
         if self.image is not None:
             grayscale_image = cv2.cvtColor(np.array(self.image), cv2.COLOR_RGB2GRAY)
 
-            # Инициализация начального порога
-            threshold = 128
+            percentile = self.threshold_scale2.get()
+            threshold = np.percentile(grayscale_image, percentile)
 
             # Максимальное количество итераций
             max_iterations = 100
@@ -117,18 +173,16 @@ class ImageProcessingApp:
             segmented_image = Image.fromarray(binary_image)
             self.display_image(segmented_image)
 
-    from sklearn.cluster import KMeans
-
-    def apply_k_means(self):
+    # Метод к-средних
+    def k_means_segmentation(self):
         if self.image is not None:
             grayscale_image = cv2.cvtColor(np.array(self.image), cv2.COLOR_RGB2GRAY)
 
-            # Инициализация параметров k-средних
-            k_values = [2, 3, 4, 5]  # Различные значения k для сравнения
-            best_threshold = None
-            best_score = float('inf')
+            k_values = [2, 3, 4, 5]
 
-            for k in k_values:
+            fig, axes = plt.subplots(1, len(k_values), figsize=(16, 6))
+
+            for i, k in enumerate(k_values):
                 # Применение метода k-средних
                 kmeans = KMeans(n_clusters=k, random_state=0).fit(grayscale_image.reshape(-1, 1))
                 labels = kmeans.labels_
@@ -137,31 +191,19 @@ class ImageProcessingApp:
                 # Вычисление среднеквадратичного отклонения от центроидов
                 score = np.mean((grayscale_image.reshape(-1, 1) - centers[labels]) ** 2)
 
-                # Выбор наилучшего порога на основе минимального среднеквадратичного отклонения
-                if score < best_score:
-                    best_score = score
-                    best_threshold = np.mean(centers)
+                # Бинаризация изображения по лучшему порогу
+                binary_image = np.where(grayscale_image > np.mean(centers), 255, 0).astype(np.uint8)
 
-            # Бинаризация изображения по лучшему порогу
-            binary_image = np.where(grayscale_image > best_threshold, 255, 0).astype(np.uint8)
+                # Отображение сегментированного изображения
+                segmented_image = Image.fromarray(binary_image)
 
-            # Отображение сегментированного изображения
-            segmented_image = Image.fromarray(binary_image)
-            self.display_image(segmented_image)
+                # Отображение на одном холсте
+                axes[i].imshow(segmented_image, cmap='gray')
+                axes[i].set_title(f'(k={k}), Отклонение: {score:.2f}')
+                axes[i].axis('off')
 
-    def open_image(self):
-        self.image_path = filedialog.askopenfilename(filetypes=[("Image Files", "*.png;*.jpg;*.jpeg;*.gif;*.bmp")])
-        if self.image_path:
-            self.image = Image.open(self.image_path)
-            self.display_image()
-
-    def display_image(self, image=None):
-        if image is None and self.image_path:
-            image = Image.open(self.image_path)
-        if image:
-            image = image.resize((600, 600), Image.Resampling.LANCZOS)
-            self.tk_image = ImageTk.PhotoImage(image)
-            self.canvas.create_image(300, 300, image=self.tk_image)
+            plt.tight_layout()
+            plt.show()
 
     def apply_threshold(self):
         if self.image is not None:
@@ -182,59 +224,6 @@ class ImageProcessingApp:
             binarized_image = binarize_image(grayscale_array, threshold)
             binarized_image = Image.fromarray(binarized_image)
             self.display_image(binarized_image)
-
-    def apply_segmentation(self):
-        if self.image is not None:
-            grayscale_image = cv2.cvtColor(np.array(self.image), cv2.COLOR_RGB2GRAY)
-
-            # Applying Sobel edge detection
-            sobel_x = cv2.Sobel(grayscale_image, cv2.CV_64F, 1, 0, ksize=3)
-            sobel_y = cv2.Sobel(grayscale_image, cv2.CV_64F, 0, 1, ksize=3)
-            magnitude = np.sqrt(sobel_x ** 2 + sobel_y ** 2)
-
-            # Binarizing the edge image
-            threshold = 50  # You can adjust this threshold as needed
-            binary_image = np.where(magnitude >= threshold, 255, 0).astype(np.uint8)
-
-            # Displaying the segmented image
-            segmented_image = Image.fromarray(binary_image)
-            self.display_image(segmented_image)
-
-    # пункт 1
-    def segment_by_edge_detection(self):
-        # Преобразование изображения в оттенки серого
-        grayscale_image = cv2.cvtColor(self.image, cv2.COLOR_RGB2GRAY)
-
-        # Применение алгоритма выделения краев
-        edges = cv2.Canny(grayscale_image, 100, 200)  # Пример использования оператора Кэнни
-
-        # Бинаризация краевого изображения
-        binary_image = np.where(edges > 0, 255, 0).astype(np.uint8)
-
-        # Преобразование в объект Image для отображения
-        segmented_image = Image.fromarray(binary_image)
-
-        return segmented_image
-
-    # пункт P-tile
-    def apply_segmentation2(self):
-        if self.image is not None:
-            # Преобразование изображения в оттенки серого
-            grayscale_image = cv2.cvtColor(np.array(self.image), cv2.COLOR_RGB2GRAY)
-
-            # Вычисление порога на основе процентиля (P-tile)
-            percentile = self.threshold_scale2.get()
-            threshold = np.percentile(grayscale_image, percentile)
-
-            # Бинаризация изображения по порогу
-            binary_image = np.where(grayscale_image >= threshold, 255, 0).astype(np.uint8)
-
-            # Отображение сегментированного изображения
-            segmented_image = Image.fromarray(binary_image)
-            self.display_image(segmented_image)
-
-
-import matplotlib.pyplot as plt
 
 
 def calculate_and_plot_histogram(image):
@@ -271,7 +260,7 @@ def binarize_image(image, threshold):
 import scipy.signal
 
 
-def compare_smoothing(image, smooth_levels=[1, 3, 5,10,15]):
+def compare_smoothing(image, smooth_levels=[1, 3, 5, 10, 15]):
     histogram, _ = np.histogram(image.flatten(), bins=256, range=(0, 256))
 
     for level in smooth_levels:
